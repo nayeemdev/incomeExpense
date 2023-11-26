114a0eb935a028d8cac5e6ed5b323de1240ea016
Md Nayeem Hossain <nayeem9812@gmail.com>
2023-11-26 15:17:37 +0600

    Revert "add thousand separator"
    This reverts commit ca2a3b245a58802df08721027cae1ab7ece5cfcd.

--- app/Helpers/global.php
+++
@@ -1,5 +0,0 @@
-<?php
-    // format add thousand separator and define 
-    function format_amount($number) {
-        return number_format($number, config('setting.decimal_digit'),".",",");
-    }
