494e729865435d75bc766fa222c8ef5fcdb349b5
Md Nayeem Hossain <nayeem9812@gmail.com>
2023-11-26 15:17:37 +0600

    Revert "add month selection to Dashboard"
    This reverts commit 4fc8b7e1428be3e2b9d304408e401914d30c9390.

--- app/Helpers/global.php
+++ app/Helpers/global.php
@@ -1,29 +1,5 @@
 <?php
-// format add thousand separator and define 
-function format_amount($number) {
-    return number_format($number, config('setting.decimal_digit'),".",",");
-}
-
-//return array of period for the use of dropdown,
-//formated as yyyy-mm
-function list_period($until) {
-    $month = date("m")*1;
-    $year = date("Y")*1;
-
-    //TODO: get the earliest entry. Assign to $last_year & $last_month
-    //      or, assign $last_year & $last_month as param
-    $last_year = substr($until,0,4)*1;
-    $last_month = substr($until,4,2)*1;
-
-    do {
-        $period = "$year-".str_pad($month,2,'0',STR_PAD_LEFT);
-        $result[$period] = date("Y M", strtotime($period.'-01'));
-        $month--;
-        if($month==0) {
-            $month = 12;
-            $year--;
-        }
-    }while($year>=$last_year && $month>=$last_month);
-
-    return $result;
-}
+    // format add thousand separator and define 
+    function format_amount($number) {
+        return number_format($number, config('setting.decimal_digit'),".",",");
+    }
